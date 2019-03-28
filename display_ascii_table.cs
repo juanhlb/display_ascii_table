57ca50148eeb553c18b88bd6be32309dd05884fe
juanhlb <juanhlb@github.com>
2019-03-27 21:06:56 -0400

    Create display_ascii_table.cs

---
+++ display_ascii_table.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace display_ascii_table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.Write("Decimal".PadRight(10));
+        Console.Write("ASCII".PadRight(10));
+        Console.Write("Hex".PadRight(10));
+        Console.WriteLine();
+
+        int min = 0;
+        int max = 128;
+        for (int i = min; i < max; i++)
+        {
+            // Get ASCII character.
+            char c = (char)i;
+
+            // Get display string.
+            string display = string.Empty;
+            if (char.IsWhiteSpace(c))
+            {
+                display = c.ToString();
+                switch (c)
+                {
+                    case '\t':
+                        display = "\\t";
+                        break;
+                    case ' ':
+                        display = "space";
+                        break;
+                    case '\n':
+                        display = "\\n";
+                        break;
+                    case '\r':
+                        display = "\\r";
+                        break;
+                    case '\v':
+                        display = "\\v";
+                        break;
+                    case '\f':
+                        display = "\\f";
+                        break;
+                }
+            }
+            else if (char.IsControl(c))
+            {
+                display = "control";
+            }
+            else
+            {
+                display = c.ToString();
+            }
+            // Write table row.
+            Console.Write(i.ToString().PadRight(10));
+            Console.Write(display.PadRight(10));
+            Console.Write(i.ToString("X2"));
+            Console.WriteLine();
+            
+
+            }
+        Console.ReadKey();
+        }
+    }
+}
